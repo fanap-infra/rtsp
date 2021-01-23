72f034152adf7853e4e2e27459077c99a0cc1c3f
mohammad <mohamadvatandoost512@gmail.com>
2021-01-23 14:50:11 +0330

    do not change state

--- client/client.go
+++ client/client.go
@@ -435,15 +435,15 @@ func (self *Client) findRTSP() (block []byte, data []byte, err error) {
 
 		// logRTP.Tracev("rtsp: findRTSP", "i", i, "b", b)
 
-		if len(peek) > 20000000 {
+		if len(peek) > 40000000 {
 			log.Errorv("PeekLength", "len(peek)", len(peek), "len(block)", len(block))
 			peek = peek[(len(peek) / 2):]
-			stat = 0
+			// stat = 0
 		}
-		if len(block) > 20000000 {
+		if len(block) > 40000000 {
 			log.Errorv("BlockLength", "len(peek)", len(peek), "len(block)", len(block))
 			block = block[(len(block) / 2):]
-			stat = 0
+			// stat = 0
 		}
 
 		if stat != 0 {
